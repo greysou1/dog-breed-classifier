60bce8378b3487ae4eb7f7112a1b068d0ba8cf07
GreySou1 <prudvi.kamtam16@gmail.com>
2021-04-06 00:09:50 +0530

    Code clean up

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import requests
-encoded_file = open('test_examples/encoded_image.txt', 'r')
-base64_string = encoded_file.read()
-encoded_file.close()
+from PIL import Image
 
-resp = requests.post('http://dbc.my.to:8000', json={"image":base64_string})
+file_name = 'test/0d61e4885d53a0abad1112c4edfa9fda.jpg'
+
+with open(file_name, 'rb') as img:
+    image = img.read()
+
+resp = requests.post('http://dbc.my.to:8000', files={"image":image})
 
 print(resp.json())
